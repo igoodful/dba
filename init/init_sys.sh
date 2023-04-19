71bbf4e7a3fc98b80fc3ab79dd1db41600235963
igoodful <igoodful@qq.com>
2023-04-19 09:50:07 +0800

    Update init_sys.sh
    add proxysql.repo

--- init/init_sys.sh
+++ init/init_sys.sh
@@ -12,7 +12,14 @@ EOF
 echo 8192 > /proc/sys/net/core/somaxconn
 
 
-
+cat > /etc/yum.repos.d/proxysql.repo << EOF
+[proxysql]
+name=ProxySQL YUM repository
+baseurl=https://repo.proxysql.com/ProxySQL/proxysql-2.4.x/centos/\$releasever
+gpgcheck=1
+gpgkey=https://repo.proxysql.com/ProxySQL/proxysql-2.4.x/repo_pub_key
+EOF
+# yum install proxysql OR yum install proxysql-version
 
 
 
